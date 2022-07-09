2ab09478415f47cbdc51d4350a141625b33124e5
RiyaMondal9211 <108669389+RiyaMondal9211@users.noreply.github.com>
2022-07-09 11:57:54 +0530

    Add files via upload

---
+++ IncrementAndDecrementDemo.java
@@ -0,0 +1,13 @@
+package basicprogram;
+
+public class IncrementAndDecrementDemo {
+
+	public static void main(String args[]) {
+		
+		int x=10,y;
+		y=++x;
+		System.out.println("x value: "+x);
+		System.out.println("y value: "+y);
+		
+	}
+}
---
+++ TernaryOperatorDemo.java
@@ -0,0 +1,12 @@
+package basicprogram;
+
+public class TernaryOperatorDemo {
+	public static void main(String args[])
+	{
+		
+		int a=100,b=20,max;
+		max=(a>b)?a:b;
+		System.out.println(max+ " is greater");
+	}
+
+}
